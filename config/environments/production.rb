3c735fe4ae76d47eb613b9e7799ff7beda7dea12
Martin Gonzalez <tinchogon34@gmail.com>
2014-07-03 12:36:38 -0300

    TLD Length

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_dispatch.tld_length = 2
+  #config.action_dispatch.tld_length = 1
 end
